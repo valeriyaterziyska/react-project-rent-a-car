6010fb0f7f467f44776ffc6f7b05f49e6814cc1a
Valeria Terziyska <valeria.terziyska@gmail.com>
2024-08-15 20:05:25 +0300

    Change html

--- client/src/components/AddCar/AddCar.js
+++ client/src/components/AddCar/AddCar.js
@@ -2,13 +2,13 @@ export const AddCar = () => {
   return (
     <section id="createPage">
       <form id="createForm">
-        <label for="brand">Brand:</label>
+        <label htmlFor="brand">Brand:</label>
         <input type="text" id="brand" name="brand" placeholder="Brand..." />
 
-        <label for="model">Model:</label>
+        <label htmlFor="model">Model:</label>
         <input type="text" id="model" name="model" placeholder="Model..." />
 
-        <label for="imageUrl">Image:</label>
+        <label htmlFor="imageUrl">Image:</label>
         <input
           type="text"
           id="imageUrl"
@@ -16,13 +16,13 @@ export const AddCar = () => {
           placeholder="Image..."
         />
 
-        <label for="price">Price:</label>
+        <label htmlFor="price">Price:</label>
         <input type="number" id="price" name="price" placeholder="Price..." />
 
-        <label for="price">Test drive option:</label>
+        <label htmlFor="price">Test drive option:</label>
         <input type="checkbox" id="testDrive" name="testDrive" />
 
-        <button type="submit" class="create">
+        <button type="submit" className="create">
           Create Car
         </button>
       </form>
